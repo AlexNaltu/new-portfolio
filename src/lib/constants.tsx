b76d403a2aa7e2053b37a7883fb4e04fa22d91c9
Alex Naltu <naltuok@gmail.com>
2024-10-28 08:07:11 +0100

    created constants file for data

---
+++ src/lib/constants.tsx
@@ -0,0 +1,14 @@
+export const heroInfo = [
+  {
+    title: "Full-Stack Developer",
+  },
+  {
+    title: "Germany",
+  },
+  {
+    title: "Available to work",
+  },
+  {
+    title: "Gym Enthusiast",
+  },
+];
