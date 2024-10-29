b5031236d2227cf3508feebe65b94dee8d95c2c1
Alex Naltu <naltuok@gmail.com>
2024-10-29 07:00:14 +0100

    new constants for skills, services and stack

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,14 +1,103 @@
+import { MdDeveloperBoard, MdEventAvailable } from "react-icons/md";
+import { HiLocationMarker } from "react-icons/hi";
+import { RiTimeZoneFill, RiTailwindCssFill } from "react-icons/ri";
+import { CgGym } from "react-icons/cg";
+import { SiNextdotjs, SiTypescript } from "react-icons/si";
+import { FaReact } from "react-icons/fa6";
+
 export const heroInfo = [
   {
     title: "Full-Stack Developer",
+    icon: <MdDeveloperBoard size={20} />,
   },
   {
     title: "Germany",
+    icon: <HiLocationMarker size={20} />,
+  },
+  {
+    title: "UTC+01",
+    icon: <RiTimeZoneFill size={20} />,
   },
   {
     title: "Available to work",
+    icon: <MdEventAvailable size={20} />,
   },
   {
     title: "Gym Enthusiast",
+    icon: <CgGym size={20} />,
+  },
+];
+
+export const services = [
+  {
+    service: "Full-Stack Web App",
+  },
+  {
+    service: "Frontend Development",
+  },
+  {
+    service: "Backend Development",
+  },
+  {
+    service: "Database Management",
+  },
+  {
+    service: "Web Performance ",
+  },
+  {
+    service: "Testing and Debugging",
+  },
+  {
+    service: "Deployment & Hosting",
+  },
+];
+
+export const skills = [
+  "React",
+  "Next.Js",
+  "Angular",
+  "JavaScript",
+  "TypeScript",
+  "HTML",
+  "CSS",
+  "SCSS",
+  "Tailwind CSS",
+  "Express.Js",
+  "Node.Js",
+  "Shadcn-Ui",
+  "MongoDB",
+  "Prisma",
+  "Supabase",
+  "& More Libraries",
+];
+
+export const techStacks = [
+  {
+    title: "Next.Js",
+    icon: <SiNextdotjs size={20} />,
+  },
+  {
+    title: "React",
+    icon: <FaReact size={20} />,
+  },
+  {
+    title: "Tailwind CSS",
+    icon: <RiTailwindCssFill size={20} />,
+  },
+  {
+    title: "TypeScript",
+    icon: <SiTypescript size={20} />,
+  },
+];
+
+export const frameworks = [
+  {
+    image: "/icons/React.png",
+  },
+  {
+    image: "/icons/Angular.png",
+  },
+  {
+    image: "/icons/Next.png",
   },
 ];
