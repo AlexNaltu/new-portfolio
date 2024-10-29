d6596bbf96b9931ec9b317595cdc4319a7be1866
Alex Naltu <naltuok@gmail.com>
2024-10-29 06:59:36 +0100

    added new layout style and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Hero from "@/components/hero/hero";
+import Services from "@/components/services/services";
+import Skills from "@/components/skills/skills";
+import TechStacks from "@/components/tech-stack/tech-stack";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Services />
+      <Skills />
+      <TechStacks />
     </div>
   );
 }
