28a87f742cb1df72415d8ecca75e1cd9ca846974
Alex Naltu <naltuok@gmail.com>
2024-10-29 07:00:52 +0100

    completed the skills component

---
+++ src/components/skills/skills.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { skills } from "@/lib/constants";
+import { SiHyperskill } from "react-icons/si";
+
+const Skills = () => {
+  return (
+    <div className="mt-4">
+      <Card className="p-4">
+        <CardHeader>
+          <CardTitle className="font-black text-xl flex items-center gap-2">
+            <SiHyperskill size={27} className="text-tertiary" /> Skills
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-grow flex-wrap gap-3 text-xs mt-2">
+          {skills.map((skill, i) => (
+            <h1 key={i} className="flex-shrink">
+              {skill}
+            </h1>
+          ))}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Skills;
