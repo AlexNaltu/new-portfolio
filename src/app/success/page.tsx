ca74a699708fb3fd108757a15dec91d3d2e33bab
Alex Naltu <naltuok@gmail.com>
2024-10-30 17:42:34 +0100

    created success page

---
+++ src/app/success/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuccessPage = () => {
+  return (
+    <div className="text-center mt-10 text-tertiary">
+      Email sent successfully, I will get back to you soon.
+    </div>
+  );
+};
+
+export default SuccessPage;
