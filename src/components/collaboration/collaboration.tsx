22e326ee1af82ea0bd8b433f3dc6b0af1c3cbd52
Alex Naltu <naltuok@gmail.com>
2024-12-16 06:35:49 +0100

    added my discord server link

--- src/components/collaboration/collaboration.tsx
+++ src/components/collaboration/collaboration.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { MdBusinessCenter } from "react-icons/md";
 import { Button } from "../ui/button";
 import Contact from "../contact/contact";
+import { BsDiscord } from "react-icons/bs";
 
 const Collaboration = () => {
   return (
@@ -12,14 +13,20 @@ const Collaboration = () => {
           <MdBusinessCenter size={40} className="text-tertiary mx-auto " />
         </CardHeader>
         <CardContent>
-          <h1 className="font-black">Let &apos; s Collaborate</h1>
+          <h1 className="font-black">Let&apos;s Collaborate</h1>
           <p className="mb-5 text-xs text-slate-300">
             Building Success Through Collaboration
           </p>
           <div className="flex flex-col gap-2">
             <Contact />
-            <Link href="/portfolio.pdf" download="Portfolio" target="_blank">
-              <Button className="w-full bg-[#151414]">Download CV</Button>
+            <Link
+              href="https://discord.gg/vq3TBTRpzE"
+              download="Portfolio"
+              target="_blank"
+            >
+              <Button className="w-full bg-[#151414]">
+                <BsDiscord size={30} /> Discord Server
+              </Button>
             </Link>
           </div>
         </CardContent>
